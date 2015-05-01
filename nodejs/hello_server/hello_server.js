b77e3f24c4322e5f69c6c3f6550f59c43d66aa0c
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2015-05-01 18:49:52 +0500

    update to server

--- nodejs/hello_server/hello_server.js
+++ nodejs/hello_server/hello_server.js
@@ -2,7 +2,12 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (request, response){
+app.get('/:requestId', function (request, response){
+	var requestId  = request.params.requestId;
+	response.json(request[requestId]);
+});
+
+app.get('/:headerName/:headerContent', function (request, response){
 	response.send('Hello Express');
 });
 
